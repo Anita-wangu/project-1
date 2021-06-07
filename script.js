d39dd9e3233c14b2b4440cc44bbccd180fc8c2c3
Anita Wangu <nitawangu@gmail.com>
2021-06-07 12:58:41 +0300

    update added a function to add two numbers

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 
-function words( str ) {
-    return String( str )
-        .toLowerCase()
-        .split( /\s|\b/ )
-        .filter( function aplha( v ) {
-            return /^[\w]+$/.test( v );
-        } );
-}
-
-function unique( list ) {
-    let uniqList = [];
-
-    for ( let v of list ) {
-        if ( uniqList.indexOf( v ) === -1 ) {
-            uniqList.push( v );
-        }
-    }
-    return uniqList;
+function add(a,b){
+    return a+b
 }
